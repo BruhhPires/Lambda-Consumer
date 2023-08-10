ea4ff099254af4a5ae7933667afa60162b75865e
Bruno Pires <brunopires.ctto@gmail.com>
2023-08-10 10:53:54 -0300

    Inline Lambda expression

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,11 +18,8 @@ public class Program {
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
-		
-		
-		Consumer<Product> cons = p -> p.setPrice(p.getPrice()*1.1);
-		
-		list.forEach(cons);
+
+		list.forEach( p -> p.setPrice(p.getPrice()*1.1) ); // EXPRESSÃO LAMBDA DECLARADA DENTRO DO ARGUMENTO (INLINE)
 		
 		list.forEach(System.out::println);
 	
