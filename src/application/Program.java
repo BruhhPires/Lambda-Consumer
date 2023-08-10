e0049d8dd62401893711243fa869e2e96cd25f1a
Bruno Pires <brunopires.ctto@gmail.com>
2023-08-10 10:52:34 -0300

    Declared Lambda expression

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,9 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entities.Product;
-import util.PriceUpdate;
 
 public class Program {
 
@@ -19,7 +19,10 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::nonStaticPriceUpdate);
+		
+		Consumer<Product> cons = p -> p.setPrice(p.getPrice()*1.1);
+		
+		list.forEach(cons);
 		
 		list.forEach(System.out::println);
 	
