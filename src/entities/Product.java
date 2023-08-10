b025e2f85d2dbd31763f3ff1a0995b01cab5dbc7
Bruno Pires <brunopires.ctto@gmail.com>
2023-08-10 10:49:44 -0300

    Method Reference non static

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -29,6 +29,10 @@ public class Product {
 	public static void staticPriceUpdate(Product p) {
 		p.setPrice(p.getPrice()*1.1);
 	}
+	
+	public void nonStaticPriceUpdate() {
+		price = price * 1.1;
+	}
 
 	@Override
 	public String toString() {
